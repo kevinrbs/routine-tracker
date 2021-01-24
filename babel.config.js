362038c805ad51edfc6f84f7a2c96c6de96e9626
Kevin Santos <kevin.santos@codemettle.com>
2021-01-24 12:45:30 -0500

    added aliases

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            pages: './src/pages/index.ts',
+            components: './src/components/index.ts',
+            layouts: './src/layouts/index.ts',
+            assets: './src/assets/index.ts',
+            utils: './src/utils/index.ts',
+            styles: './src/styles/index.ts',
+          },
+        },
+      ],
+    ],
   };
 };
